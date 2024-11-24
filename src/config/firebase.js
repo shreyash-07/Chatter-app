9c5eeb96243321fbc20ccadb28d6045b7c775f0c
Shreyash <shreyashanindya3@gmail.com>
2024-11-24 12:39:35 +0530

    Completed Profile UI

---
+++ src/config/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyBF-rvTgdov3xbUdG5Z5mOYgIIGuh1Yk-M",
+  authDomain: "chatter-app-4d160.firebaseapp.com",
+  projectId: "chatter-app-4d160",
+  storageBucket: "chatter-app-4d160.firebasestorage.app",
+  messagingSenderId: "823190420917",
+  appId: "1:823190420917:web:bb5580877f320c83f32dfc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);  
